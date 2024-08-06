651f4ef71c74d38c9545bbdb9d8f6c6e27e1abf9
ChesterPZH <166971022+ChesterPZH@users.noreply.github.com>
2024-08-06 23:23:03 +0800

    Add files via upload

---
+++ 1DOF_Hapkit.ino
@@ -0,0 +1,192 @@
+// Includes
+#include <math.h>
+
+// Pin declares
+int pwmPin = 5; // PWM output pin for motor 1
+int dirPin = 8; // direction output pin for motor 1
+int sensorPosPin = A2; // input pin for MR sensor
+int fsrPin = A3; // input pin for FSR sensor
+
+// Position tracking variables
+int updatedPos = 0;     // keeps track of the latest updated value of the MR sensor reading
+int rawPos = 0;         // current raw reading from MR sensor
+int lastRawPos = 0;     // last raw reading from MR sensor
+int lastLastRawPos = 0; // last last raw reading from MR sensor
+int flipNumber = 0;     // keeps track of the number of flips over the 180deg mark
+int tempOffset = 0;
+int rawDiff = 0;
+int lastRawDiff = 0;
+int rawOffset = 0;
+int lastRawOffset = 0;
+const int flipThresh = 700;  // threshold to determine whether or not a flip over the 180 degree mark occurred
+boolean flipped = false;
+
+// Kinematics variables
+double xh = 0;           // position of the handle [m]
+double theta_s = 0;      // Angle of the sector pulley in deg
+double xh_prev;          // Distance of the handle at previous time step
+double xh_prev2;
+double dxh;              // Velocity of the handle
+double dxh_prev;
+double dxh_prev2;
+double dxh_filt;         // Filtered velocity of the handle
+double dxh_filt_prev;
+double dxh_filt_prev2;
+
+double force = 0;           // force at the handle
+double Tp = 0;              // torque of the motor pulley
+double duty = 0;            // duty cylce (between 0 and 255)
+unsigned int output = 0;    // output command to the motor
+
+void setup() {
+  // Start the serial communication
+  Serial.begin(115200);
+
+  // Set PWM frequency 
+  setPwmFrequency(pwmPin,1); 
+  
+  // Input pins
+  pinMode(sensorPosPin, INPUT); // set MR sensor pin to be an input
+  pinMode(fsrPin, INPUT);       // set FSR sensor pin to be an input
+
+  // Output pins
+  pinMode(pwmPin, OUTPUT);  // PWM pin for motor A
+  pinMode(dirPin, OUTPUT);  // dir pin for motor A
+
+  pinMode(13, OUTPUT);
+  
+  // Initialize motor 
+  analogWrite(pwmPin, 0);     // set to not be spinning (0/255)
+  digitalWrite(dirPin, LOW);  // set direction
+  
+  // Initialize position valiables
+  lastLastRawPos = analogRead(sensorPosPin);
+  lastRawPos = analogRead(sensorPosPin);
+}
+
+void loop() {
+
+  rawPos = analogRead(sensorPosPin);  //current raw position from MR sensor
+
+  // Calculate differences between subsequent MR sensor readings
+  rawDiff = rawPos - lastRawPos;          //difference btwn current raw position and last raw position
+  lastRawDiff = rawPos - lastLastRawPos;  //difference btwn current raw position and last last raw position
+  rawOffset = abs(rawDiff);
+  lastRawOffset = abs(lastRawDiff);
+  
+  // Update position record-keeping vairables
+  lastLastRawPos = lastRawPos;
+  lastRawPos = rawPos;
+  
+  // Keep track of flips over 180 degrees
+  if((lastRawOffset > flipThresh) && (!flipped)) { // enter this anytime the last offset is greater than the flip threshold AND it has not just flipped
+    if(lastRawDiff > 0) {        // check to see which direction the drive wheel was turning
+      flipNumber--;              // cw rotation 
+    } else {                     // if(rawDiff < 0)
+      flipNumber++;              // ccw rotation
+    }
+    if(rawOffset > flipThresh) { // check to see if the data was good and the most current offset is above the threshold
+      updatedPos = rawPos + flipNumber*rawOffset; // update the pos value to account for flips over 180deg using the most current offset 
+      tempOffset = rawOffset;
+    } else {                     // in this case there was a blip in the data and we want to use lastactualOffset instead
+      updatedPos = rawPos + flipNumber*lastRawOffset;  // update the pos value to account for any flips over 180deg using the LAST offset
+      tempOffset = lastRawOffset;
+    }
+    flipped = true;            // set boolean so that the next time through the loop won't trigger a flip
+  } else {                        // anytime no flip has occurred
+    updatedPos = rawPos + flipNumber*tempOffset; // need to update pos based on what most recent offset is 
+    flipped = false;
+  }
+
+  double rh = 0.15;
+  double ts = -0.00674 * updatedPos + 1.5;
+  xh = radians(ts) * rh;
+  // Serial.println(xh, 5);
+
+  dxh = (double)(xh - xh_prev) / 0.001;
+
+  // Calculate the filtered velocity of the handle using an infinite impulse response filter
+  dxh_filt = .9*dxh + 0.1*dxh_prev; 
+    
+  // Record the position and velocity
+  xh_prev2 = xh_prev;
+  xh_prev = xh;
+  
+  dxh_prev2 = dxh_prev;
+  dxh_prev = dxh;
+  
+  dxh_filt_prev2 = dxh_filt_prev;
+  dxh_filt_prev = dxh_filt;
+
+  if (Serial.available() > 0) {
+    // Read the incoming data as a string
+    String data = Serial.readStringUntil('\n');
+    // Optionally convert to a float if needed
+    double phyphox_data = data.toFloat();
+    // Print the received data to the serial monitor
+    // Serial.println(phyphox_data);
+    double k_stiff = 20;
+    force = 0.01 * phyphox_data * k_stiff;
+    Serial.print(phyphox_data);
+    Serial.print('-');
+    Serial.println(force, 5);
+  }
+
+  double rp = 0.0047625;   //[m]
+  double rs = 0.085;   //[m]
+  double jacob = rh * rp / rs;
+  Tp = jacob * force;
+
+    // Determine correct direction for motor torque
+  if(force > 0) { 
+    digitalWrite(dirPin, HIGH);
+  } else {
+    digitalWrite(dirPin, LOW);
+  }
+
+  // Compute the duty cycle required to generate Tp (torque at the motor pulley)
+  duty = sqrt(abs(Tp)/0.03);
+
+  // Make sure the duty cycle is between 0 and 100%
+  if (duty > 1) {            
+    duty = 1;
+  } else if (duty < 0) { 
+    duty = 0;
+  }  
+  output = (int)(duty* 255);   // convert duty cycle to output signal
+  analogWrite(pwmPin,output);  // output the signal
+}
+
+// --------------------------------------------------------------
+// Function to set PWM Freq -- DO NOT EDIT
+// --------------------------------------------------------------
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x7; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
